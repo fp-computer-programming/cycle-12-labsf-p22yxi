ad490c156d13f56ec3ccc0ced52fb53a41161497
p22yxi <p22yxi@fairfieldprep.org>
2022-02-15 10:20:25 -0500

    lab12

---
+++ lab_12-1.py
@@ -0,0 +1,7 @@
+# Yongdong Xi
+ofile = open("my_file.txt", "w")
+# Create a handle to open a file for writing called my_file.txt
+ofile.write("xyd \n")
+ofile.write("Hello World! \n")
+ofile.write("Playing guitar \n")
+ofile.close()
---
+++ lab_12-2.py
@@ -0,0 +1,6 @@
+# Yongdong Xi
+ofile = open("my_file.txt", "a")
+# Use the append method to add 2 more lines to my_file.txt
+ofile.write("Use phone to have a face time with my family and friends \n")
+ofile.write("travelling \n")
+ofile.close()
